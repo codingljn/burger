2c3df3e7632ac32b78eca4119278ac05cee08ccd
Codn Ljn <lujan2@gmail.com>
2018-08-04 09:55:02 -0400

    updated burgers.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -27,7 +27,7 @@ $(function() {
   
       var newBurger = {
         burger_name: $("#ca").val().trim(),
-        devoured: 0//$("[name=devoured]:checked").val().trim()
+        devoured: 0
       };
   
       // Send the POST request.
