7f3f3cadf7ec4497c734be37c20b6b4d0356ad23
Codn Ljn <lujan2@gmail.com>
2018-08-03 23:28:09 -0400

    added working version

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Angus Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Ostrich Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Spicy Burger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Da B.I.G Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Mystery Burger', true);
+
+SELECT * FROM burger;
